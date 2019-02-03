8f746df2c6bf95224b7d01ea054b691f2835bf9e
Zachery Gentry <zacherygentry@gmail.com>
2019-02-02 21:49:23 -0600

    lab fin

--- Lab3 - Music/hw02.py
+++ Lab3 - Music/hw02.py
@@ -10,43 +10,45 @@ def getFrequency(keyNumber):
     return float(440 * np.power(2, (keyNumber - 49)/12))
 
 
-def writeNote(keyNumber, length=.5):
+def writeNote(keyNumber, bassKey=-1, length=.5):
     sample = 8000
     x = np.arange(sample * length)
-    freq = getFrequency(keyNumber)
-    y = np.sin(2 * np.pi * freq * x / sample)
-    
+    y = np.sin(2 * np.pi * getFrequency(keyNumber) * x / sample)
+    if bassKey != -1:
+        y2 = np.sin(2 * np.pi * getFrequency(bassKey - 24) * x / sample)
+    else:
+        y2 = 0
+
     pos = f.tell()
     f.seek(pos)
 
-    f.write(y)
+    f.write(y + y2)
 
 
-
+d = {'C': 52, 'D': 54, 'E': 56, 'F': 57, 'G': 59, 'A': 61, 'B': 63}
 ### M A I N ###
-writeNote(52) # C
-writeNote(52) # C
-writeNote(59) # G
-writeNote(59) # G
-writeNote(61) # A
-writeNote(61) # A
-writeNote(59) # G
-writeNote(59) # G
-writeNote(57) # F
-writeNote(57) # F
-writeNote(56) # E
-writeNote(56) # E
+writeNote(d['C'])  # C
+writeNote(d['C'])  # C
+writeNote(d['G'])  # G
+writeNote(d['G'])  # G
+writeNote(d['A'])  # A
+writeNote(d['A'])  # A
+writeNote(d['G'])  # G
+writeNote(d['G'])  # G
+writeNote(d['F'])  # F
+writeNote(d['F'])  # F
+writeNote(d['E'])  # E
+writeNote(d['E'])  # E
 
-writeNote(54) # D
-writeNote(54) # D
-writeNote(56) # E
-writeNote(52) # C
-writeNote(59) # G
-writeNote(59) # G
-writeNote(57) # F
-writeNote(57) # F
-writeNote(56) # E
-writeNote(56) # E
-writeNote(54) # D
-writeNote(54) # D
-
+writeNote(d['D'])  # D
+writeNote(d['D'])  # D
+writeNote(d['E'])  # E
+writeNote(d['C'])  # C
+writeNote(d['G'])  # G
+writeNote(d['G'])  # G
+writeNote(d['F'])  # F
+writeNote(d['F'])  # F
+writeNote(d['E'])  # E
+writeNote(d['E'])  # E
+writeNote(d['D'])  # D
+writeNote(d['D'])  # D
