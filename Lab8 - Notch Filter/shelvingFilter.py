f9f638a73bb6dc7725dc210280ec2933e4aa59dc
Zachery Gentry <zacherygentry@gmail.com>
2019-04-17 14:32:52 -0500

    ugh

--- Lab8 - Notch Filter/shelvingFilter.py
+++ Lab8 - Notch Filter/shelvingFilter.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import read
 from scipy.io.wavfile import write
+from scipy.signal import lfilter
 
 
 def applyShelvingFilter(inName, outName, g, fc):
@@ -13,57 +14,73 @@ def applyShelvingFilter(inName, outName, g, fc):
     # From their docs
     # https://plot.ly/matplotlib/fft/
     k = np.arange(N)
-    print(np.max(audio))
     T = N/fs
     frq = k / T
-    frq = frq[range(int(N/2))]
+    frq = frq[range(int(N/4))]
     Y = np.fft.fft(audio).real / N
-    Y = Y[range(int(N/2))]
-    magn = np.max(Y)
-    plt.ylim(0, magn + 100)
+    Y = Y[range(int(N/4))]
+    magn1 = np.max(Y)
     plt.subplot(1, 2, 1)
     plt.plot(frq, abs(Y))
 
     # gain
-    mu = 10 ** (g / 20)
-    Oc = 2 * np.pi * fc / fs
-    num = 1 - ( 4 / ( 1 + mu) ) * np.tan(Oc / 2)
-    den = 1 + ( 4 / ( 1 + mu) ) * np.tan(Oc / 2)
-    gamma = num / den
-    alpha = (1 - gamma) / 2
+    b, a = shelvingFilter(g, fc, fs)
+    newAudio = lfilter(b, a, audio)
+    Y = np.fft.fft(newAudio).real / N
+    Y = Y[range(int(N / 4))]
+    magn2 = np.max(Y)
 
-    u = [0]
-    y = []
-    for index, x in enumerate(audio):
-        if index < 1:
-            continue
-        
-        # u(n) section
-        temp = alpha * (x + audio[index - 1])
-        if index == 1:
-            u.append(temp)
-        else:
-            temp = temp + gamma * u[index - 1]
-            u.append(temp)
-        # y(n) section
-        temp = x + (mu - 1) * u[index]
-        y.append(temp)
-
-    shelvingOutput = np.convolve(audio, y)
-    print(shelvingOutput)
+    if magn1 > magn2:
+        magn = magn1
+    else:
+        magn = magn2
+    plt.ylim(0, magn + 100)
     plt.subplot(1, 2, 2)
-    Y = np.fft.fft(shelvingOutput).real / N
-    Y = Y[range(int(N / 2))]
-    magn = np.max(shelvingOutput)
+    plt.ylim(0, magn + 100)
     plt.plot(frq, abs(Y))
-    write(outName, fs, shelvingOutput)
+
+    write(outName, fs, newAudio)
     plt.show()
+
+
+def shelvingFilter(g, fc, fs):
+    K = np.tan((np.pi * fc)/fs)
+    V0 = 10 ** (g / 20)
+    root2 = np.sqrt(2)
+
+    if V0 < 1:
+        V0 = 1/V0
+
+    if g > 0:
+        b0 = (V0 + root2*np.sqrt(V0)*K + K**2) / (1 + root2*K + K**2)
+        b1 = (2 * (K**2 - V0)) / (1 + root2*K + K**2)
+        b2 = (V0 - root2*np.sqrt(V0)*K + K**2) / (1 + root2*K + K**2)
+        a1 = (2 * (K**2 - 1)) / (1 + root2*K + K**2)
+        a2 = (1 - root2*K + K**2) / (1 + root2*K + K**2)
+    elif g < 0:
+        b0 = (1 + root2*K + K**2) / (V0 + root2*np.sqrt(V0)*K + K**2)
+        b1 = (2 * (K**2 - 1)) / (V0 + root2*np.sqrt(V0)*K + K**2)
+        b2 = (1 - root2*K + K**2) / (V0 + root2*np.sqrt(V0)*K + K**2)
+        a1 = (2 * ((K**2)/V0 - 1)) / (1 + root2/np.sqrt(V0)*K + (K**2)/V0)
+        a2 = (1 - root2/np.sqrt(V0)*K + (K**2)/V0) / \
+            (1 + root2/np.sqrt(V0)*K + (K**2)/V0)
+    else:
+        b0 = V0
+        b1 = 0
+        b2 = 0
+        a1 = 0
+        a2 = 0
+
+    b = [b0, b1, b2]
+    a = [1, a1, a2]
+
+    return b, a
 
 
 ##########################  main  ##########################
 if __name__ == "__main__":
     inName = "P_9_1.wav"
-    gain = -5  # can be positive or negative
+    gain = 2  # can be positive or negative
     # WARNING: small positive values can greatly amplify the sounds
     cutoff = 300
     outName = "shelvingOutput.wav"
